fbbff9a6837842e5af571b8bbd26736945aac55a
apensia914 <apensia914@gmail.com>
2021-05-13 22:42:04 +0900

    Router & Controller setting

--- src/server.js
+++ src/server.js
@@ -1,34 +1,16 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouters";
+import userRouter from "./routers/userRouters";
 
 const app = express();
 const port = 4000;
 
-const somethingMiddleware = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if (url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    console.log("Allowed, you may continue");
-    next();
-}
-
-app.use(somethingMiddleware);
-app.use(privateMiddleware);
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
-app.get("/protected", (req, res) => {
-    return res.send("Welcome to private lounge.")
-})
-
-app.get("/login", (req, res) => {
-    res.send("Please login here!");
-})
+app.use(morgan("dev"));
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
